8710cf384714ffb59035877bc490d992bfd983af
Karolis Riaubūnas <64730811+Karka-Admin@users.noreply.github.com>
2022-11-26 18:11:07 +0200

    paturbinimas

--- Zaidimas_2/main.cpp
+++ Zaidimas_2/main.cpp
@@ -1,7 +1,12 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 #include <iostream>
-#include <sstream>
+#include <vector>
+#include <utility>
 #include <random>
+
+#define ENEMY_COUNT	5
 
 void playerMovement(sf::RectangleShape &player)
 {
@@ -13,14 +18,14 @@ void playerMovement(sf::RectangleShape &player)
 	{
 		player.move(-5.f, 0.f);
 	}
-
+	
 	/*else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && player.getPosition().y > -1)
 	{
-		player.move(0.f, -1.f);
+		player.move(0.f, -5.f);
 	}
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && player.getPosition().y < 1030)
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && player.getPosition().y < 950)
 	{
-		player.move(0.f, 1.f);
+		player.move(0.f, 5.f);
 	}*/
 }
 
@@ -42,50 +47,138 @@ float randomizerSpeed()
 	return float(distr(gen));
 }
 
+void checkWindowEvents(sf::RenderWindow &window)
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+	{
+		window.close();
+	}
+}
+
 int main()
 {
 	// Langas
 	sf::RenderWindow window(sf::VideoMode(1000, 1000), "Avoid the rain!", sf::Style::Close);
+	window.setPosition(sf::Vector2i(0, 10));
 	window.setFramerateLimit(300);
 
 	// Veikejas
 	sf::Vector2f playerSize(50.f, 50.f);
 	sf::RectangleShape player(playerSize);
-	player.setFillColor(sf::Color::Yellow);
+	player.setFillColor(sf::Color::Red);
 	player.setPosition(0.f, 850.f);
 
 	// Priesai
+	std::pair<float, float> enemyProperties[ENEMY_COUNT];
+	for (int i = 0; i < ENEMY_COUNT; i++)
+	{
+		enemyProperties[i].first = randomizerX();
+		enemyProperties[i].second = randomizerSpeed();
+	}
+
 	sf::Vector2f enemySize(50.f, 50.f);
 	sf::RectangleShape enemy(enemySize);
-	enemy.setFillColor(sf::Color::Red);
+	enemy.setFillColor(sf::Color::Cyan);
 
-	// Taskai
-	uint64_t score = 0;
-	
+	std::vector <sf::RectangleShape> enemies(ENEMY_COUNT);
+	for (int i = 0; i < ENEMY_COUNT; i++)
+	{
+		enemies[i] = enemy;
+		enemies[i].setPosition(randomizerX(), 0.f);
+	}
 
-	float randomX = 0, randomSpeed = 2.f;
+	// TEKSTAS
+	sf::Font font;
+	if (!font.loadFromFile("font.ttf"))
+	{
+		std::cerr << "ERROR\n";
+		return -1;
+	}
+
+	// "GAME OVER!"
+	sf::Text gameOverText;
+	gameOverText.setFont(font);
+	gameOverText.setString("GAME OVER!");
+	gameOverText.setCharacterSize(100);
+	gameOverText.setFillColor(sf::Color::Red);
+	gameOverText.setOrigin
+	(
+		gameOverText.getLocalBounds().left + gameOverText.getGlobalBounds().width / 2.0f,
+		gameOverText.getLocalBounds().top + gameOverText.getGlobalBounds().height / 2.0f
+	);
+	gameOverText.setPosition(500.f, 500.f);
+
+	// "PRADETI ZAIDIMA?"
+	sf::Text gameStartText;
+	gameStartText.setFont(font);
+	gameStartText.setString("PRESS ENTER TO START");
+	gameStartText.setCharacterSize(72);
+	gameStartText.setFillColor(sf::Color::Red);
+	gameStartText.setOrigin
+	(
+		gameStartText.getLocalBounds().left + gameStartText.getGlobalBounds().width / 2.0f,
+		gameStartText.getLocalBounds().top + gameStartText.getGlobalBounds().height / 2.0f
+	);
+	gameStartText.setPosition(500.f, 500.f);
+
+	// PRADZIOS LANGAS
+	window.draw(gameStartText);
+	window.display();
+
+	while (!sf::Keyboard::isKeyPressed(sf::Keyboard::Enter) && window.isOpen())
+	{
+		checkWindowEvents(window);
+	}
 
 	while (window.isOpen())
 	{
+		// LANGO VALDYMAS
+		checkWindowEvents(window);
+
+		// LIETAUS JUDEJIMAS/VALDYMAS
+		for (int i = 0; i < ENEMY_COUNT; i++)
+		{
+			if (enemies[i].getPosition().y > 950.f)
+			{
+				enemyProperties[i].first = randomizerX();
+				enemyProperties[i].second = randomizerSpeed();
+
+				enemies[i].setPosition(enemyProperties[i].first, 0.f);
+			}
+		}
+
+		for (int i = 0; i < ENEMY_COUNT; i++)
+		{
+			enemies[i].setPosition(enemyProperties[i].first, enemies[i].getPosition().y + enemyProperties[i].second);
+		}
+
+		for (int i = 0; i < ENEMY_COUNT; i++)
+		{
+			if (player.getGlobalBounds().intersects(enemies[i].getGlobalBounds()))
+			{
+				window.draw(gameOverText);
+				window.display();
+
+				while (window.isOpen())
+				{
+					checkWindowEvents(window);
+				}
+			}
+		}
+
+		// ZAIDEJO JUDEJIMAS/VALDYMAS
 		playerMovement(player);
 
-		if (enemy.getPosition().y > 950.f)
-		{
-			randomX = randomizerX();
-			randomSpeed = randomizerSpeed();
-
-			enemy.setPosition(randomX, 0.f);
-		}
-		enemy.setPosition(randomX, enemy.getPosition().y + randomSpeed);
-
+		// PIESIMAS
 		window.clear();
-
 		window.draw(player);
-		window.draw(enemy);
+		
+		for (int i = 0; i < ENEMY_COUNT; i++)
+		{
+			window.draw(enemies[i]);
+		}
 
 		window.display();
-		
-		score++;
 	}
 
 	return 0;
